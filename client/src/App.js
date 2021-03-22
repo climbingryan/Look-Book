681c390717f9a53d878df0b6d06910cd65c8f003
shaggy <simmons5family@gmail.com>
2021-03-21 22:21:18 -0500

    add apollo server to app.js in client

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { ApolloPervider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+import { getOperationAST } from 'graphql';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({ 
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
+
+function App() {
+  return (
+    <ApolloClient client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloClient>
+  );
+}
+
+export default App;
